cfec434b2170894cccc2ed301606d9a6fcd77736
u7352872006-web <u7352872006@gmail.com>
2026-03-18 17:38:21 +0000

    Update closer-select.js

--- closer-select.js
+++ closer-select.js
@@ -1,4 +1,4 @@
-const DATA_URL = "https://script.google.com/macros/s/YOUR_DEPLOYED_URL/exec"; // ←最新デプロイURLに置き換える
+const DATA_URL = "https://script.google.com/macros/s/AKfycbxb282oIXg6UrpqJ1MM2txXEriwJnq8nHiUFqZTpyoI8FJ4zOHFjrQKvqnDhteA9qTl/exec"; // ←最新デプロイURLに置き換える
 
 const RANK_ORDER = ["トップセールス", "2軍", "3軍", "研修生", "審査落ち"];
 
